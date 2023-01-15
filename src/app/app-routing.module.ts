8d2acd0c94229e144f8be1dbd1b2cc676d3c6c00
t0ng42013 <tonga88@live.com.ar>
2023-01-15 09:11:10 -0300

    probando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CardDeskComponent } from './header/card-desk/card-desk.component';
-import { ExpComponent } from './header/card-desk/exp/exp.component';
 import { AuthGuard } from './helper/auth.guard';
-import { ModalsComponent } from './modals/modals.component';
+
 
 const routes: Routes = [
     {
         path: '',
         component: AppComponent,
         canActivate: [AuthGuard]
-    },
-    {
-        path:'experiencia' , component:ExpComponent,canActivate:[AuthGuard]
-    },
-    {
-        path: 'login',
-        component: ModalsComponent
-
     }
+   
+ 
 ];
 
 @NgModule({
